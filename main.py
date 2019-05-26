606855effe4d73769ceacea2b54f0b6aaa22755b
Michael Pavlov <mr.michael.pavlov@gmail.com>
2019-05-26 20:18:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import telebot
+from datetime import datetime
+from flask import Flask, request
+
+API_TOKEN = os.environ['TOKEN']
+bot = telebot.TeleBot(API_TOKEN)
+
+server = Flask(__name__)
+TELEBOT_URL = 'telebot_webhook/'
+BASE_URL = 'https://test-hw-bot.herokuapp.com/'
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    reply_with_log(message, "Hi there, I am EchoBot. Just say anything nice and I'll say the exact same thing to you!")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    reply_with_log(message, message.text)
+
+
+@server.route('/' + TELEBOT_URL + API_TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=BASE_URL + TELEBOT_URL + API_TOKEN)
+    return "!", 200
+
+
+webhook()
+server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
