3c9e9838aacde2fec41ec099fa05c55dec5b6ae3
Michael Pavlov <mr.michael.pavlov@gmail.com>
2019-05-26 20:28:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ BASE_URL = 'https://test-hw-bot.herokuapp.com/'
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    reply_with_log(message, "Hi there, I am EchoBot. Just say anything nice and I'll say the exact same thing to you!")
+    bot.reply_to(message, "Hi there, I am EchoBot. Just say anything nice and I'll say the exact same thing to you!")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    reply_with_log(message, message.text)
+    bot.reply_to(message, message.text)
 
 
 @server.route('/' + TELEBOT_URL + API_TOKEN, methods=['POST'])
