54a393baa4bdca3dca509192e3bd4974ecf9528c
Michael Pavlov <mr.michael.pavlov@gmail.com>
2019-05-26 22:44:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import telebot
-from datetime import datetime
 from flask import Flask, request
 
 API_TOKEN = os.environ['TOKEN']
@@ -13,27 +12,29 @@ BASE_URL = 'https://test-hw-bot.herokuapp.com/'
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hi there, I am EchoBot. Just say anything nice and I'll say the exact same thing to you!")
+    bot.reply_to(message, "Hi! test version 1.1")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text + "_test version 1.1_", parse_mode='Markdown')
 
 
-@server.route('/' + TELEBOT_URL + API_TOKEN, methods=['POST'])
+# @server.route('/' + TELEBOT_URL + API_TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
 
-@server.route("/")
+# @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=BASE_URL + TELEBOT_URL + API_TOKEN)
     return "!", 200
 
+server.add_url_rule('/' + TELEBOT_URL + API_TOKEN,view_func=get_message,methods=['POST'])
+server.add_url_rule("/",view_func=webhook)
 
 webhook()
 server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
