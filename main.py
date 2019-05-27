d07dcf657a7e3ce65032c74b796828b3076bddc5
Michael Pavlov <mr.michael.pavlov@gmail.com>
2019-05-27 23:42:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ class SaleMonBot:
         self.logger.setLevel(logging.DEBUG)
         handler = logging.StreamHandler(sys.stdout)
         handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s %(name)s : %(levelname)s : %(message)s')
+        formatter = logging.Formatter('%(name)s: %(levelname)s: %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
 
